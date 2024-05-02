890bade4c1e20f34c3e932761a0957670b93162b
zip95297 <zip95297@gmail.com>
2024-05-02 19:56:03 +0800

    测试pruned模型

--- recognition/model/__init__.py
+++ recognition/model/__init__.py
@@ -5,3 +5,4 @@ from .loss import FocalLoss
 from .resnet18 import ResNet18
 from .ShuffleNetv2 import ShuffleNetV2
 from .MobileNetV2 import MobileNetV2 , mobile_half
+from .resnet18_with_cfg import ResNet18_with_config
